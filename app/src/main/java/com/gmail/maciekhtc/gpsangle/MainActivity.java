a7dee7f408f8e2b5e8d35ee63aa6bb7ac5c04017
Maciej Sieradzki <Maciej Sieradzki>
2016-05-24 10:43:45 +0200

    Created git repo

---
+++ app/src/main/java/com/gmail/maciekhtc/gpsangle/MainActivity.java
@@ -0,0 +1,102 @@
+package com.gmail.maciekhtc.gpsangle;
+
+import android.location.Location;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {       //implements OnMapReadyCallback
+
+    Button addMarkerButton;
+    Button calculateButton;
+    GoogleMap mMap;
+    LinkedHashMap<Marker,Location> mPoints = new LinkedHashMap<Marker,Location>();
+    TextView textView1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapka);
+        mapFragment.getMapAsync(this);
+
+        textView1 = (TextView) findViewById(R.id.textView1);
+
+
+        addMarkerButton = (Button) findViewById(R.id.addMarkerButton);
+        addMarkerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MarkerOptions newMarker = new MarkerOptions();
+                Location newLoc = mMap.getMyLocation();
+                LatLng newLatLng = MyUtils.latlngFromLocation(newLoc);
+                newMarker.position(newLatLng);
+                newMarker.draggable(true);
+                newMarker.title(newLatLng.toString());
+                mPoints.put(mMap.addMarker(newMarker), newLoc);
+                //textView1.setText(newLatLng.toString());
+            }
+        });
+
+
+        calculateButton = (Button) findViewById(R.id.calculateButton);
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //action                                            !!
+                Location[] locArr = mPoints.values().toArray(new Location[mPoints.values().size()]);
+                textView1.setText("Pole= "+ MyUtils.calculateField(locArr));
+            }
+        });
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMyLocationEnabled(true);
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(13), 150, null);
+        mMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+            @Override
+            public void onMyLocationChange(Location location) {
+                mMap.animateCamera(CameraUpdateFactory.newLatLng(MyUtils.latlngFromLocation(location)));
+            }
+        });
+        mMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+            @Override
+            public void onMarkerDragStart(Marker marker) {
+
+            }
+
+            @Override
+            public void onMarkerDrag(Marker marker) {
+
+            }
+
+            @Override
+            public void onMarkerDragEnd(Marker marker) {
+                marker.setTitle(marker.getPosition().toString());
+                Location markerlocation = mPoints.get(marker);
+                markerlocation.setLatitude(marker.getPosition().latitude);
+                markerlocation.setLongitude(marker.getPosition().longitude);
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/gmail/maciekhtc/gpsangle/MyUtils.java
@@ -0,0 +1,61 @@
+package com.gmail.maciekhtc.gpsangle;
+
+import android.location.Location;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.List;
+
+
+/**
+ * Created by 15936 on 01.05.2016.
+ */
+public class MyUtils {
+    public static Location locFromLatLng(LatLng input)
+    {
+        Location result = new Location("");
+        result.setLatitude(input.latitude);
+        result.setLongitude(input.longitude);
+        return result;
+    }
+    public static LatLng latlngFromLocation(Location input)
+    {
+        return new LatLng(input.getLatitude(),input.getLongitude());
+    }
+    public static double fieldOfTriangle(double lengthA, double lengthB, double lengthC)
+    {
+        double lengthP=(lengthA+lengthB+lengthC)/2;
+        return Math.sqrt(lengthP*(lengthP-lengthA)*(lengthP-lengthB)*(lengthP-lengthC));
+    }
+    public static double fieldFromLocations(Location loc1, Location loc2, Location loc3)
+    {
+        return (fieldOfTriangle(loc1.distanceTo(loc2),loc2.distanceTo(loc3),loc3.distanceTo(loc1)));
+    }
+    public static Location middleLocation(Location[] listOfPoints)
+    {
+        Location result=new Location(listOfPoints[0]);      //przykladowy punkt do modyfikacji, bo ciezko stworzyc od zera
+        double sumLatitudes=0, sumLongitudes=0, sumAltitudes=0;
+        for (Location loc : listOfPoints)
+        {
+            sumLatitudes += loc.getLatitude();
+            sumLongitudes += loc.getLongitude();
+            sumAltitudes += loc.getAltitude();
+        }
+        int quantity = listOfPoints.length;
+        result.setLatitude(sumLatitudes / quantity);
+        result.setLongitude(sumLongitudes / quantity);
+        result.setAltitude(sumAltitudes / quantity);
+        return result;
+    }
+    public static double calculateField(Location[] listOfPoints)
+    {
+        double result=0;
+        Location middlePoint = middleLocation(listOfPoints);
+        for (int i=0;i<listOfPoints.length-1;i++)
+        {
+            result+=fieldFromLocations(middlePoint,listOfPoints[i],listOfPoints[i+1]);
+        }
+        result+=fieldFromLocations(middlePoint,listOfPoints[0],listOfPoints[listOfPoints.length-1]);
+        return result;
+    }
+}
